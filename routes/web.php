62bc89a0f89f4d6e72a8100a4093c73840d1480b
Anonimo <bselhorst88@gmail.com>
2024-01-11 10:05:27 -0500

    changed routes

--- routes/web.php
+++ routes/web.php
@@ -15,31 +15,22 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->middleware(['auth'])->name('home');
+// All authenticated routes
+Route::middleware('auth')->group(function() {
+    // Main route
+    Route::get('/', function () {
+        return view('home');
+    });
 
-Route::get('/user', function () {
-    return view('user.index');
-})->middleware(['auth'])->name('user.index');
-
-Route::prefix('users')->group(function () {
-    Route::post('/', [UserController::class, 'store'])->name('user.store');
-    Route::get('/', [UserController::class, 'index'])->name('user.index');
-    Route::get('/create', [UserController::class, 'create'])->name('user.create');
-    Route::get('/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
-    Route::patch('/edit/{id}', [UserController::class, 'update'])->name('user.update');
-    Route::delete('/{id}', [UserController::class, 'destroy'])->name('user.destroy');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Users routes
+    Route::prefix('users')->group(function () {
+        Route::post('/', [UserController::class, 'store'])->name('user.store');
+        Route::get('/', [UserController::class, 'index'])->name('user.index');
+        Route::get('/create', [UserController::class, 'create'])->name('user.create');
+        Route::get('/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+        Route::patch('/edit/{id}', [UserController::class, 'update'])->name('user.update');
+        Route::delete('/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
